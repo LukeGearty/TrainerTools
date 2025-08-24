ce0f7ac6bd0d99cc34f3b7af451c7c372b18ba07
LukeGearty <lukegearty@gmail.com>
2025-08-24 19:20:25 -0400

    changing structure

--- trainertools/core/views.py
+++ trainertools/core/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'core/dashboard.html')
+
+# Create your views here.
--- trainertools/trainertools/urls.py
+++ trainertools/trainertools/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path
-from . import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path("", include("core.urls")),
+
 ]
