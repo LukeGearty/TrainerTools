0e4b2064ddcdfb5741d5fa0dfafab1717ce28e43
LukeGearty <lukegearty@gmail.com>
2025-08-25 12:58:34 -0400

    worked on showing clients in the dashboard

--- trainertools/core/models.py
+++ trainertools/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#Trainer will be the default user
+
+class Client(models.Model):
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="clients")
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- trainertools/core/views.py
+++ trainertools/core/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Client
 
 
 
 @login_required
 def dashboard(request):
-    return render(request, 'core/dashboard.html')
+    clients = Client.objects.filter(trainer=request.user)
+    return render(request, 'core/dashboard.html', {"clients": clients})
 
-# Create your views here.
