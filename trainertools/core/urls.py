0a0de80cf8372ab2d4c4bd27984e73a70bb5fbaa
LukeGearty <lukegearty@gmail.com>
2025-08-25 15:19:17 -0400

    added a user registration

--- trainertools/core/urls.py
+++ trainertools/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import RegisterView
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="core/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("register/", RegisterView.as_view(), name="register")
 ]
--- trainertools/core/views.py
+++ trainertools/core/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Client
-
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
 
 
 @login_required
@@ -9,3 +11,7 @@ def dashboard(request):
     clients = Client.objects.filter(trainer=request.user)
     return render(request, 'core/dashboard.html', {"clients": clients})
 
+class RegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = "core/register.html"
+    success_url = reverse_lazy("login")
